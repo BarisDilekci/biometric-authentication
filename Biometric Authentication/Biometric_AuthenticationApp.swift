11788d1a216119996fed8fba82a4743485d3e710
BarisDilekci <barixmenuygulama@gmail.com>
2024-11-06 01:50:03 +0300

    Initial Commit

---
+++ Biometric Authentication/Biometric_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometric_AuthenticationApp.swift
+//  Biometric Authentication
+//
+//  Created by Barış Dilekçi on 6.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Biometric_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
